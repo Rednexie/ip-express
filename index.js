535867a82bddda4fca76996eae2360e52a7538f9
Rednexie <126991413+Rednexie@users.noreply.github.com>
2023-06-19 01:49:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
 
+const http = require("http")
+const cors = require("cors");
+const express = require("express")
+const useragent = require("express-useragent")
+const app = express();
+
+// Modules
+const consoled = require("consoled.js"); // I am in love with this module, since it's made by me(narcissistic rizz)
+
+app.use(cors())
+
+app.use(useragent.express())
+
+app.use((req, res, next) => {
+  const ipp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const ip = ipp.replace("::ffff:", "")
+  const browser = req.useragent.browser;
+  const version = req.useragent.version;
+  const os = req.useragent.os;
+  const method = req.method;
+  const url = req.originalUrl
+  const base = req.baseurl;
+  const time = new Date().toLocaleString("tr-TR");
+  const host = req.hostname;
+  const requestPath = req.path;
+  consoled.bright.green(`IP Address: ${ip}, Browser: ${browser} ${version}, OS: ${os}, method: ${method}, time: ${time}, ${requestPath} to ${host}`);
+  next();
+});
+
+// --------------------------------------------------------------------------------------------------------------------
+//            IP 
+//                     V4
+//                               API  
+// --------------------------------------------------------------------------------------------------------------------
+app.get("/api", (req, res) => {
+  const ipp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const ip;
+  if(ipp == "::1"){
+    ip = "localhost"
+    return res.status(200).send(ip)
+  }
+  ip = ipp.replace("::ffff:", "");
+  return res.status(200).send(ip)
+})
+
+app.get("/text", (req, res) => {
+  const ipp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const ip = ipp.replace("::ffff:", "")
+  res.status(200).send(ip)
+})
+app.get("/json", (req, res) => {
+  const ipp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const ip = ipp.replace("::ffff:", "")
+  res.json(ip)
+})
+
+app.get("/json/:id", (req, res) => {
+  const ipp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const ip = ipp.replace("::ffff:", "")
+  res.json(`${req.params.id}: ${ip}`);
+})
+
+
+
+const httpServer = http.createServer(app)
+const server = httpServer.listen(80, () => {
+  consoled.blue("HTTP Server created on port " + server.address().port)
+})
