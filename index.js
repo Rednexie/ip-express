68f7acec0852ef4efb8c2ac5edb1e09a843662ed
Rednexie <126991413+Rednexie@users.noreply.github.com>
2024-01-17 09:32:05 +0000

    new file:   .gitignore 	modified:   index.js 	new file:   package-lock.json 	new file:   package.json

--- index.js
+++ index.js
@@ -1,30 +1,64 @@
-const cors = require("cors");
-const express = require("express")
-const useragent = require("express-useragent")
-const app = express();
-const port = 80;
-// Modules
-const consoled = require("consoled.js");
+const app = require("express")()
+const fs = require('fs');
 
-app.use(cors())
 
-app.use(useragent.express())
+
+const port = 3000;
+const log = true
+
+
+app.set('trust proxy', true)
+app.set('json spaces', 2);
+app.set('etag', false);
+app.disable('x-powered-by');
+
+app.use(require('cors')())
+
 
 app.use((req, res, next) => {
-  const ipp = req.connection.remoteAddress || req.headers['x-forwarded-for'];;
-  const ip = ipp.replace("::ffff:", "")
-  const browser = req.useragent.browser;
-  const version = req.useragent.version;
-  const os = req.useragent.os;
-  const method = req.method;
-  const url = req.originalUrl
-  const base = req.baseurl;
-  const time = new Date().toLocaleString("tr-TR");
-  const host = req.hostname;
-  const requestPath = req.path;
-  consoled.bright.green(`IP Address: ${ip}, Browser: ${browser} ${version}, OS: ${os}, method: ${method}, time: ${time}, ${requestPath} to ${host}`);
+    
+  res.setHeader('X-Content-Type-Options', 'nosniff')
+  res.setHeader('x-xss-protection', '1; mode=block');
+  res.setHeader('x-frame-options', false);
+  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+  res.setHeader('Cross-Origin-Resource-Policy', 'same-site');
+  res.setHeader('Origin-Agent-Cluster', 'true');
+  res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
+  res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('X-DNS-Prefetch-Control', 'off');
+  res.setHeader('X-Download-Options', 'noopen');
+  res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('X-Permitted-Cross-Domain-Policies', 'none');
+  res.setHeader('X-XSS-Protection', '1; mode=block');
+  res.removeHeader('X-Ratelimit-Limit');
+  res.removeHeader('X-Ratelimit-Remaining');
+  res.removeHeader('X-Ratelimit-Reset');
+  res.setHeader('Set-Cookies', 'SameSite=None; Secure');
+  res.setHeader('X-Frame-Options', 'DENY');
+
+
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'HEAD,GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
+
+
+
+
+
+
+app.use((req, res, next) => {
+  if(log){
+    fs.appendFile('./logs', `[${new Date().toLocaleString()}] ${req.ip}: ${req.method} => ${req.originalUrl}`, (err) => {
+        if(err) console.error(err)
+    })
+  }
+  console.log(`[${new Date().toLocaleString()}] ${req.ip}: ${req.method} => ${req.originalUrl}`)
+  next()
+})
 
 // --------------------------------------------------------------------------------------------------------------------
 //            IP 
@@ -32,25 +66,25 @@ app.use((req, res, next) => {
 //                               API  
 // --------------------------------------------------------------------------------------------------------------------
 app.get("/api", (req, res) => {
-  const ipp = req.connection.remoteAddress || req.headers['x-forwarded-for'];
-  const ip;
+  let ip;
+  const ipp = req.ip;
   if(ipp == "::1"){
-    ip = "localhost"
+    ip = "127.0.0.1"
     return res.status(200).send(ip)
   }
   ip = ipp.replace("::ffff:", "");
-  return res.status(200).send(ip)
+  return res.status(200).json({ ip })
 })
 
 app.get("/text", (req, res) => {
-  const ipp = req.connection.remoteAddress || req.headers['x-forwarded-for'];
+  const ipp = req.ip;
   const ip = ipp.replace("::ffff:", "")
   res.status(200).send(ip)
 })
 app.get("/json", (req, res) => {
-  const ipp = req.connection.remoteAddress || req.headers['x-forwarded-for'];
+  const ipp = req.ip;
   const ip = ipp.replace("::ffff:", "")
-  res.json(ip)
+  res.json({ip})
 })
 
 app.get("/json/:id", (req, res) => {
@@ -63,7 +97,8 @@ app.get("/json/:id", (req, res) => {
 })
 
 
-
-const server = app.listen(port, () => {
-  consoled.blue("HTTP Server created on port " + server.address().port)
+app.listen(port, () => {
+  console.log('ip-express is listening on port', port)
 })
+
+if(log && !fs.existsSync('./logs')) fs.writeFileSync('./logs', '', 'utf-8')
